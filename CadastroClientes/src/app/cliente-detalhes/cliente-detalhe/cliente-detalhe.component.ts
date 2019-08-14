3e65731f8540e7719a1e7a51a402bbb8e937c93d
Eduardo de Oliveira Zacarias (Infraestrutura TI ADIQ - Terceiro) <eduardo.zacarias@adiq.com.br>
2019-08-14 14:13:04 -0300

    Formatado FrontEnd do projeto

--- CadastroClientes/src/app/cliente-detalhes/cliente-detalhe/cliente-detalhe.component.ts
+++ CadastroClientes/src/app/cliente-detalhes/cliente-detalhe/cliente-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ClienteDetalheService } from 'src/app/compartilhado/cliente-detalhe.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-detalhe',
@@ -11,6 +12,22 @@ export class ClienteDetalheComponent implements OnInit {
   constructor(private service:ClienteDetalheService) { }
 
   ngOnInit() {
+    this.resetForm();
+
+  }
+
+  resetForm(form?: NgForm){
+    if(form!=null)
+      form.resetForm();
+      this.service.formData = {
+          ClienteId :0,
+          NomeCompleto :'',
+          ClienteCod :'',
+          TelCliente :'',
+          ClienteCPF :'string',
+          ClienteRG :'string'
+    }
+
   }
 
 }
